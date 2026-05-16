df1ce8be05bc88263cdffc5c0c4648454c3f56fe
yamauchi <272127211+yamauchi97@users.noreply.github.com.>
2026-05-16 14:24:02 +0900

    HTMLを返す変更

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 const http = require('node:http');
+const html = `
+<!DOCTYPE html>
+<html lang="ja">
+  <body>
+    <h1>HTMLの一番大きい見出しを表示します</h1>
+  </body>
+</html>
+`;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    "Content-Type": "text/html; charset=utf-8",
   });
-  res.write(req.headers['user-agent']);
+  res.write(html);
   res.end();
 });
 const port = 8000;
